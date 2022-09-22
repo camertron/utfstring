2d85e9831475fba18c0df6e4fe8b16ca23d42748
Krisztian Balla <krisztian.balla@invaris.com>
2022-09-22 15:55:30 +0200

    Expose UtfVisualString in window object

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { UtfVisualString } from "./utf_visual_string";
 // if there is a DOM add the object to the window object
 if (typeof window !== "undefined" && window !== null) {
     (window as any).UtfString = UtfString;
+    (window as any).UtfVisualString = UtfVisualString;
 }
 
 export { UtfString, UtfVisualString };
