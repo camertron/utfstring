b1f20b7de5b75016d0821d6a199e21e79ae5db14
ErickSkrauch <erickskrauch@ely.by>
2024-04-08 19:47:24 +0200

    Simplify window detection (#19)
    Check for `typeof window` instead of using the `isDefined` utility to avoid needing JSDom in jest tests.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 /// <reference lib="DOM" />
 import { UtfString } from "./utf_string";
 import { UtfVisualString } from "./utf_visual_string";
-import { isDefined } from "./utils";
 
 // if there is a global window object add the classes to it
-if (isDefined(window)) {
+if (typeof window !== 'undefined') {
     (window as any).UtfString = UtfString;
     (window as any).UtfVisualString = UtfVisualString;
 }
